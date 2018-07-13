c54466be664a1d3087be4ee0cae59bc1062534e5
Joe Istas <joe.istas@istastech.com>
2018-07-12 20:12:56 -0500

    Added cloudfront config to serverless.yml and moved config values into variables.yml

---
+++ README.md
@@ -0,0 +1,3 @@
+# HeroesIndex Api
+
+Respostory for [api.heroesindex.com](api.heroesindex.com)
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,23 @@ const Router = require("koa-router")
 const v1Router = require("./v1")
 const router = new Router()
 
+function buildRedirectPath(to) {
+  const parts = [ process.env.LATEST_VERSION, "doc" ]
+  if(process.env.STAGE !== 'production') {
+    parts.unshift(process.env.STAGE)
+  }
+
+  return "/" + parts.join("/")
+}
+
 function redirectToDoc(ctx, next) {
-  ctx.redirect("/" + [ process.env.STAGE, process.env.LATEST_VERSION, "doc" ].join("/"))
+  ctx.redirect(buildRedirectPath("doc"))
 }
 router.get("/", redirectToDoc)
 router.get("/doc", redirectToDoc)
 
 router.get("/openapi.yaml", (ctx, next) => {
-  ctx.redirect("/" + [ process.env.STAGE, process.env.LATEST_VERSION, "openapi.yaml" ].join("/"))
+  ctx.redirect(buildRedirectPath("openapi.yaml"))
 })
 
 router.use("/v1", v1Router.routes(), v1Router.allowedMethods())
